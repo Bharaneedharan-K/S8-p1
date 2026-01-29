a2220a5bf5748e1d885b5ad7560b2162e608cfe8
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2026-01-29 10:55:39 +0530

    host URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = '/api'; // Localhost / Proxy
+const API_BASE_URL = 'https://s8-p1.onrender.com/api'; // Production Backend
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
