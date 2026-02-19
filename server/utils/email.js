82529d76fd9763af38d9d587cf1e8c1511aa0425
Bharaneedharan-K <bharaneedharan2004@gmail.com>
2026-02-19 10:46:35 +0530

    mail test

--- server/utils/email.js
+++ server/utils/email.js
@@ -10,11 +10,17 @@ const getTransporter = () => {
     }
 
     transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
+      // Increase timeout for slow connections
+      connectionTimeout: 10000, // 10 seconds
+      greetingTimeout: 5000,
+      socketTimeout: 10000,
     });
   }
   return transporter;
